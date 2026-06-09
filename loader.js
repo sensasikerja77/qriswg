9eeafa279e85826acddd39c590fa8ba01ddabbfc
sensasikerja77 <sensasikerja77@gmail.com>
2026-06-09 12:48:12 +0700

    loader.js

--- loader.js
+++ loader.js
@@ -3,11 +3,10 @@
   const store = params.get('store');
   if (!store) return;
 
-  const RAW_URL = 'https://gist.githubusercontent.com/sensasikerja77/028a62a9b788046d81dac61df710f36c/raw/25ba4263880120f63193027061fb688220f31d71/config.json'; // ⚠️ GANTI
+  const RAW_URL = 'https://gist.githubusercontent.com/sensasikerja77/028a62a9b788046d81dac61df710f36c/raw/25ba4263880120f63193027061fb688220f31d71/config.json'; // GANTI
 
   try {
     const res = await fetch(RAW_URL + '?t=' + Date.now());
-    if (!res.ok) return;
     const config = await res.json();
     const storeData = config.stores[store];
     if (!storeData) return;
